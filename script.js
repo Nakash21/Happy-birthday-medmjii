68961c8f7b923a7f8b7bdf7a936bec73e4a471e3
Nakash21 <147526902+Nakash21@users.noreply.github.com>
2025-08-20 23:49:41 +0530

    miss

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!", "medmjii"],
+    strings: ["HAPPY", "BIRTHDAY!", "MedmJii.."],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
